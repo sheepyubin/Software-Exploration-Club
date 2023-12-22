b0f6148458cf5dadd4f5c0c07cc9220fc0d4cb92
sheepyubin <yangyubinstory@naver.con>
2023-12-22 18:22:48 +0900

    디버깅 수정
    .

--- ServerProject/Assets/Script/SeverLauncher.cs
+++ ServerProject/Assets/Script/SeverLauncher.cs
@@ -12,19 +12,20 @@ public class SeverLauncher : MonoBehaviourPunCallbacks
 
     void Start()
     {
+        // 포톤 서버 접속
         PhotonNetwork.ConnectUsingSettings();
     }
 
     // 포톤 서버 연결 시 콜백
     public override void OnConnectedToMaster()
     {
-        Debug.Log("서버 연결");
+        Debug.Log("Server connection");
     }
 
     // 포톤 서버 로비 입장 시 콜백
     public override void OnJoinedRoom()
     {
-        Debug.Log("로비 입장");
+        Debug.Log("Lobby entrance");
 
         PhotonNetwork.LoadLevel("InGame");
     }
@@ -32,13 +33,13 @@ public class SeverLauncher : MonoBehaviourPunCallbacks
     // 로비 생성 시 콜백
     public override void OnCreatedRoom()
     {
-        Debug.Log("로비 생성: " + LobbyName);
+        Debug.Log("Create lobby: " + LobbyName);
     }
 
     // 로비 생성 매서드 (버튼 UI)
     public void CreateAndJoinRoom()
     {
-        Debug.Log("생성 및 입장");
+        Debug.Log("Create and enter");
         int randomNum = UnityEngine.Random.Range(1, 1000);
         string roomName = "Room " + randomNum; // 방 이름: Room 숫자
         PhotonNetwork.CreateRoom(roomName, new RoomOptions { MaxPlayers = 4 }); // 최대 접속 플레이어 4명
@@ -48,23 +49,23 @@ public class SeverLauncher : MonoBehaviourPunCallbacks
     // 로비 생성 시 갱신 콜백
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        Debug.Log("방 목록이 갱신되었습니다.");
+        Debug.Log("Update lobby list");
         foreach (RoomInfo room in roomList)
         {
-            Debug.Log("로비 이름: " + room.Name + ", 플레이어 수: " + room.PlayerCount + "/" + room.MaxPlayers);
+            Debug.Log("Lobby name: " + room.Name + ", Number of players: " + room.PlayerCount + "/" + room.MaxPlayers);
         }
     }
 
-    // 방 입장 매서드 (버튼 UI)
+    // 로비 입장 매서드 (버튼 UI)
     public void JoinRoom()
     {
-        Debug.Log("입장" + LobbyName);
+        Debug.Log("Entrance" + LobbyName);
         PhotonNetwork.JoinRandomRoom();
     }
 
-    // 로비가 존재하지 않을 때 콜백
+    // 참가 할 로비가 존재하지 않을 때 콜백
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.Log("방 없음");
+        Debug.Log("No room");
     }
 }
