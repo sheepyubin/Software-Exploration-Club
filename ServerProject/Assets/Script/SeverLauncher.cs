a50b306d54e57b737270ef883c4a67832a66298e
sheepyubin <yangyubinstory@naver.con>
2023-12-22 19:29:50 +0900

    매칭 시스템 완성
    + 자동(랜덤) 매칭 시스템
    + 수동 매칭 시스템
    + 기타 버그 수정

--- ServerProject/Assets/Script/SeverLauncher.cs
+++ ServerProject/Assets/Script/SeverLauncher.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using System;
+using System.Text.RegularExpressions;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Text;
 
 public class SeverLauncher : MonoBehaviourPunCallbacks
 {
+    [Header("---UI InputFields---")]
+    public TMP_InputField joinRoom;
+
     private string LobbyName;
 
     void Start()
     {
-        // 포톤 서버 접속
-        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.ConnectUsingSettings(); // 포톤 서버 접속
     }
 
     // 포톤 서버 연결 시 콜백
@@ -25,8 +30,8 @@ public class SeverLauncher : MonoBehaviourPunCallbacks
     // 포톤 서버 로비 입장 시 콜백
     public override void OnJoinedRoom()
     {
-        Debug.Log("Lobby entrance");
-
+        Debug.Log("Lobby entrance: " + LobbyName);
+        Debug.Log("Lobby Code: " + ExtractNumbersUsingRegex(LobbyName));
         PhotonNetwork.LoadLevel("InGame");
     }
 
@@ -37,35 +42,50 @@ public class SeverLauncher : MonoBehaviourPunCallbacks
     }
 
     // 로비 생성 매서드 (버튼 UI)
-    public void CreateAndJoinRoom()
+    public void CreateAndJoinRoom_BTN()
     {
-        Debug.Log("Create and enter");
-        int randomNum = UnityEngine.Random.Range(1, 1000);
-        string roomName = "Room " + randomNum; // 방 이름: Room 숫자
-        PhotonNetwork.CreateRoom(roomName, new RoomOptions { MaxPlayers = 4 }); // 최대 접속 플레이어 4명
-        LobbyName = roomName; // 현재 생성된 로비 이름 저장
+        string roomName = "Room " + UnityEngine.Random.Range(1, 1000); // 로비 이름 설정
+        PhotonNetwork.CreateRoom(roomName, new RoomOptions { MaxPlayers = 4 }); // 로비 생성
+
+        LobbyName = roomName;
     }
 
-    // 로비 생성 시 갱신 콜백
-    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    // 랜덤 로비 입장 매서드 (버튼 UI)
+    public void JoinRandomRoom_BTN()
     {
-        Debug.Log("Update lobby list");
-        foreach (RoomInfo room in roomList)
-        {
-            Debug.Log("Lobby name: " + room.Name + ", Number of players: " + room.PlayerCount + "/" + room.MaxPlayers);
-        }
-    }
-
-    // 로비 입장 매서드 (버튼 UI)
-    public void JoinRoom()
-    {
-        Debug.Log("Entrance" + LobbyName);
         PhotonNetwork.JoinRandomRoom();
+    }
+
+    // 지정 로비 입장 매서드 (버트 UI)
+    public void JoinDesignatedRoom_BTN()
+    {
+        if (!string.IsNullOrEmpty(joinRoom.text)) // 입력 텍스트가 NULL이 아니라면
+        {
+            string roomName = "Room " + joinRoom.text; // 로비 이름 설정
+            PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default, null); // 로비 입장
+            // 입력받은 로비로 입장 없다면 생성 후 입장   
+
+            LobbyName = roomName;
+        }
+        else
+            Debug.Log("Please enter the lobby code");
     }
 
     // 참가 할 로비가 존재하지 않을 때 콜백
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.Log("No room");
+        Debug.Log("There is no lobby \r\nPlease create a lobby");
+    }
+
+    // 문자열에서 숫자를 추출하는 매서드 (로비 코드)
+    static string ExtractNumbersUsingRegex(string input)
+    {
+        // 숫자 추출 정규식
+        string pattern = @"\d+";
+
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        // 문자열 반환
+        return string.Join("", matches);
     }
 }
