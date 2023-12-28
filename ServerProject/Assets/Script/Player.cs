9c0958f3e4f75f9762db3d74662c7d62079306aa
sheepyubin <yangyubinstory@naver.con>
2023-12-28 23:57:05 +0900

    2023/12/28
    + 플레이어 photonView 개선
    + 버그 수정

--- ServerProject/Assets/Script/Player.cs
+++ ServerProject/Assets/Script/Player.cs
@@ -1,20 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Photon.Pun;
+using UnityEngine;
 
-public class Player : MonoBehaviourPunCallbacks
+public class Player : MonoBehaviourPun
 {
     public float speed = 5f;
+
     void Update()
     {
-        if(photonView.IsMine)
+        if (photonView.IsMine)
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-
-            Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * speed * Time.deltaTime;
-            transform.Translate(movement);
+            HandleMovementInput();
         }
+    }
+
+    void HandleMovementInput()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * speed * Time.deltaTime;
+        transform.Translate(movement);
+
+        // 움직임을 다른 플레이어에게 동기화
+        if (photonView.IsMine)
+        {
+            photonView.RPC("SyncMovement", RpcTarget.Others, transform.position);
+        }
+    }
+
+    [PunRPC]
+    void SyncMovement(Vector3 newPosition)
+    {
+        // 다른 플레이어의 위치를 동기화
+        transform.position = newPosition;
     }
 }
