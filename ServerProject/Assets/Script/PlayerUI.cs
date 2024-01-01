03e68df42223756e57ceea8a5891624e4ba5b3a4
the-glory-of-the-Sun <aenigo23gi@naver.com>
2024-01-02 02:52:59 +0900

    PlayerUI 진행중
    .

--- ServerProject/Assets/Script/PlayerUI.cs
+++ ServerProject/Assets/Script/PlayerUI.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
+using Photon.Pun.Demo.PunBasics;
 
 namespace Com.MyCompany.MyGame
 {
     public class PlayerUI : MonoBehaviour
     {
         #region Private Fields
+
+        PlayerManager target;
 
         [Tooltip("UI Text to display Player's Name")]
         [SerializeField]
@@ -20,11 +23,31 @@ namespace Com.MyCompany.MyGame
         #endregion
 
         #region MonoBehaviour CallBacks
-
+        void Update()
+        {
+            // Reflect the Player Health
+            if (playerHealthSlider != null)
+            {
+                playerHealthSlider.value = target.Health;
+            }
+        }
         #endregion
 
         #region Public Methods
-
+        public void SetTarget(PlayerManager _target)
+        {
+            if (_target == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
+                return;
+            }
+            // Cache references for efficiency
+            target = _target;
+            if (playerNameText != null)
+            {
+                playerNameText.text = target.photonView.Owner.NickName;
+            }
+        }
         #endregion
 
     }
