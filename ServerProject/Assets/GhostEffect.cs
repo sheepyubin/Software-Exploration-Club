a1f98a8b113c835ae526c4762e3a60ae44c4de59
sheepyubin <yangyubinstory@naver.con>
2024-02-24 15:34:01 +0900

    대쉬 이펙트 서버 연동
    + 대쉬 이펙트 서버 연동

--- ServerProject/Assets/GhostEffect.cs
+++ ServerProject/Assets/GhostEffect.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class GhostEffect : MonoBehaviour
+public class GhostEffect : MonoBehaviourPunCallbacks, IPunObservable
 {
     public float ghostDelay;
     private float ghostDelayTime;
@@ -11,26 +12,37 @@ public class GhostEffect : MonoBehaviour
 
     void Start()
     {
-        this.ghostDelayTime = this.ghostDelay;
+        ghostDelayTime = ghostDelay;
     }
 
     void Update()
     {
-        if (this.makeGhost)
+        if (makeGhost && photonView.IsMine) // 포톤의 IsMine을 사용하여 이 로컬 플레이어가 오브젝트를 컨트롤하는지 확인
         {
-            if (this.ghostDelayTime > 0)
+            if (ghostDelayTime > 0)
             {
-                this.ghostDelayTime -= Time.deltaTime;
+                ghostDelayTime -= Time.deltaTime;
             }
             else
             {
-                GameObject currentGhost = Instantiate(this.ghost, this.transform.position, this.transform.rotation);
-                Sprite currentSprite = this.GetComponent<SpriteRenderer>().sprite;
-                currentGhost.transform.localScale = this.transform.localScale;
-                currentGhost.GetComponent<SpriteRenderer>().sprite = currentSprite;
-                this.ghostDelayTime = this.ghostDelay;
-                Destroy(currentGhost, 0.2f);
+                photonView.RPC("CreateGhost", RpcTarget.All); // 모든 플레이어에게 RPC 호출
+                ghostDelayTime = ghostDelay;
             }
         }
+    }
+
+    [PunRPC]
+    void CreateGhost()
+    {
+        GameObject currentGhost = Instantiate(ghost, transform.position, transform.rotation);
+        Sprite currentSprite = GetComponent<SpriteRenderer>().sprite;
+        currentGhost.transform.localScale = transform.localScale;
+        currentGhost.GetComponent<SpriteRenderer>().sprite = currentSprite;
+        Destroy(currentGhost, 0.2f);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+
     }
 }
