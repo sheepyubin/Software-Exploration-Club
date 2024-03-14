2b7df38c654ade7e3ef215053ebf83bceb77f27b
sheepyubin <yangyubinstory@naver.con>
2024-03-14 14:12:49 +0900

    UI 이미지 적용
    .

--- ServerProject/Assets/EscPopup.cs
+++ ServerProject/Assets/EscPopup.cs
@@ -7,6 +7,8 @@ public class EscPopup : MonoBehaviourPunCallbacks
 {
     public GameObject popupObject; // 활성화할 게임 오브젝트 변수
     public Slider volumeSlider; // 슬라이더를 가리키는 레퍼런스
+    public Image volumeImageLow; // 볼륨이 낮을 때
+    public Image volumeImageHigh; // 볼륨이 높을 때
 
 
     private int count = 0;
@@ -14,6 +16,8 @@ public class EscPopup : MonoBehaviourPunCallbacks
     private void Start()
     {
         popupObject.SetActive(false);
+
+        volumeSlider.value = 0.5f;
     }
 
     void Update()
@@ -29,6 +33,17 @@ public class EscPopup : MonoBehaviourPunCallbacks
             Disabled();
 
         AudioListener.volume = volumeSlider.value;
+
+        if (AudioListener.volume > 0.5)
+        {
+            volumeImageLow.gameObject.SetActive(false);
+            volumeImageHigh.gameObject.SetActive(true);
+        }
+        else
+        {
+            volumeImageLow.gameObject.SetActive(true);
+            volumeImageHigh.gameObject.SetActive(false);
+        }
     }
 
     public void DisabledButton()
