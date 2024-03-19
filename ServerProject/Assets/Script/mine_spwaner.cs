b32ec463a2a14b5b1317c3fd6e716ff2f890de29
PepperDoduk <ttokbeagi@gmail.com>
2024-03-19 20:54:53 +0900

    중간저장
    날라간김에 코드도 새롭게 짰읍니다
    따흑따

--- ServerProject/Assets/Script/mine_spwaner.cs
+++ ServerProject/Assets/Script/mine_spwaner.cs
@@ -5,16 +5,39 @@ using UnityEngine.TextCore.Text;
 
 public class mine_spwaner : MonoBehaviour
 {
+    public int totalMine = 5;
+
     public int fakeMine = 4;
-    public int m = -8;
-    [SerializeField]
-    private GameObject unexploded_mine;
+    public int realMine = 1;
+    public int mine_distance = -8;
+    private int now_mine=1;
+    public int realmineCount;
+
+   [SerializeField]
+    public GameObject unexploded_mine;
+    public GameObject real_mine;
     private void Start()
     {
-        for (int i = 0; i < fakeMine; ++i)
+
+        for (int i = 0; i < realMine; ++i)
         {
-            Instantiate(unexploded_mine, new Vector2(m, -31), Quaternion.identity);
-            m += 4;
+            realmineCount = Random.Range(1, 6);
+        }
+
+        for (int i = 0; i < totalMine; ++i)
+        {
+            if (now_mine != realmineCount)
+            {
+                Instantiate(unexploded_mine, new Vector2(mine_distance, -31), Quaternion.identity);
+                mine_distance += 4;
+                now_mine++;
+            }
+            else
+            {
+                Instantiate(real_mine, new Vector2(mine_distance, -31), Quaternion.identity);
+                mine_distance += 4;
+                now_mine++;
+            }
         }
     }
 
