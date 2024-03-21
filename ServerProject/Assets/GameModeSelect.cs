617087270e59110a00ab00a1cc936a6cfe2f85aa
sheepyubin <yangyubinstory@naver.con>
2024-03-21 10:09:27 +0900

    UI 개선
    + 게임 모드 선택 UI 편의성 개선

--- ServerProject/Assets/GameModeSelect.cs
+++ ServerProject/Assets/GameModeSelect.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 
 public class GameModeSelect : MonoBehaviourPunCallbacks
 {
-    string gameMode;
+    public GameObject[] Gamemodes;
+    public GameObject modeSelecter;
+
+    private string gameMode;
+
+    private void Start()
+    {
+        gameMode = "Stage_1";
+
+        modeSelecter.transform.position = Gamemodes[0].transform.position;
+    }
     public void Baisc()
     {
         gameMode = "Stage_1";
+
+        modeSelecter.transform.position = Gamemodes[0].transform.position;
     }
 
     public void Boom()
     {
         gameMode = "Boom";
+
+        modeSelecter.transform.position = Gamemodes[1].transform.position;
     }
 
     public void Lava()
     {
         gameMode = "Lava";
+
+        modeSelecter.transform.position = Gamemodes[2].transform.position;
     }
 
     public string ReturnGameMode()
