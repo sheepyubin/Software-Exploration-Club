f5953e635213a5d3967f6766c689cf6608e77d2b
PepperDoduk <ttokbeagi@gmail.com>
2024-03-29 20:45:32 +0900

    바뀐 건 없지만 일단 푸쉬
    ㅂ박대가리인지 해도 계속 오류나서 현타가 4일째 지속되고있습니다

--- ServerProject/Assets/Script/mineMode/mine_ui.cs
+++ ServerProject/Assets/Script/mineMode/mine_ui.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mineModeManager : MonoBehaviour
+public class mine_ui : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
