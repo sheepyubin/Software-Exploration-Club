06d04ab5a0762bfc72743391ac851f5d58179743
sheepyubin <yangyubinstory@naver.com>
2024-04-03 10:44:52 +0900

    버그 픽스 및 초기화 기능 추가
    + 로비 타이틀, 로딩 이미지 해상도 버그 수정
    + 모든 플레이어 사망 시 스테이지가 넘어가지 않는 버그 수정 (기본 모드)
    + 플레이어 컨테이너의 ResetContainer 매서드 추가

--- ServerProject/Assets/Script/Lobby/PlayerContainer.cs
+++ ServerProject/Assets/Script/Lobby/PlayerContainer.cs
@@ -4,42 +4,45 @@ using System;
 using System.Reflection;
 
 [CreateAssetMenu(fileName = "PlayerContainer", menuName = "ScriptableObjects/PlayerContainer", order = 1)]
+
 public class PlayerContainer : ScriptableObject
 {
     public GameObject playerPrefab; // 플레이어 프리팹(원본)
-    public Dictionary<string, GameObject> playerObject = new Dictionary<string, GameObject>(); // 키(ID), 플레이어 프리팹
-    public Dictionary<string, Color> playerColor = new Dictionary<string, Color>(); // 키(ID), 색
-    public Dictionary<string, int> playerScore = new Dictionary<string, int>(); // 키(ID), 점수
-    public Dictionary<string, bool> playerisDead = new Dictionary<string, bool>(); // 키(ID), 죽었는가?
+    public Dictionary<string, GameObject> playerObject = new Dictionary<string, GameObject>(); // playerID, 플레이어 프리팹
+    public Dictionary<string, Color> playerColor = new Dictionary<string, Color>(); // playerID, 색
+    public Dictionary<string, int> playerScore = new Dictionary<string, int>(); // playerID, 점수
+    public Dictionary<string, bool> playerisDead = new Dictionary<string, bool>(); // playerID, 죽었는가?
 
-    public void AddPlayerData(string playerID, GameObject player)
+
+    // 플레이어 데이터 추가 매서드
+    public void AddPlayerData(string playerID, GameObject player) // (playerID, 플레이어 프리팹)
     {
         playerObject[playerID] = player;
     }
 
-    public GameObject ReturnPlayerData(string playerID)
+    // 플레이어 데이터 반환 매서드
+    public GameObject ReturnPlayerData(string playerID) // (playerID)
     {
         return playerObject[playerID];
     }
 
-    public void AddPlayerColor(string playerID, Color color)
+    // 플레이어 색상 추가 매서드
+    public void AddPlayerColor(string playerID, Color color) // (playerID, 플레이어 색상)
     {
         playerColor[playerID] = color;
     }
 
-    public Color ReturnPlayerColor(string playerID)
+    // 플레이어 색상 반환 매서드
+    public Color ReturnPlayerColor(string playerID) // (playerID)
     {
-        if (playerColor.ContainsKey(playerID))
-        {
+        if (playerColor.ContainsKey(playerID)) // playerColor 딕셔너리에 playerID를 키로 가진 값이 있다면
             return playerColor[playerID];
-        }
-        else
-        {
+        else // playerColor 딕셔너리에 playerID를 키로 가진 값이 없다면
             return Color.white;
-        }
     }
 
-    public Color ReturnPlayerColorArray(int i)
+    // 플레이어 색상 배열 반환 매서드
+    public Color ReturnPlayerColorArray(int i) // index
     {
         Color[] scoresArray = new Color[playerColor.Count];
         int index = 0;
@@ -51,33 +54,28 @@ public class PlayerContainer : ScriptableObject
         }
 
         if (i >= 0 && i < scoresArray.Length)
-        {
             return scoresArray[i];
-        }
         else
-        {
             return Color.white;
-        }
     }
 
-    public void AddPlayerScore(string playerID, int score)
+    // 플레이어 점수 추가 매서드
+    public void AddPlayerScore(string playerID, int score) // (playerID, 점수)
     {
         playerScore[playerID] = score;
     }
 
-    public int ReturnPlayerScore(string playerID)
+    // 플레이어 점수 반환 매서드
+    public int ReturnPlayerScore(string playerID) // (playerID)
     {
-        if (playerScore.ContainsKey(playerID))
-        {
+        if (playerScore.ContainsKey(playerID)) // playerScore 딕셔너리에 playerID를 키로 가진 값이 있다면
             return playerScore[playerID];
-        }
-        else
-        {
+        else // playerScore 딕셔너리에 playerID를 키로 가진 값이 없다면
             return -1;
-        }
     }
 
-    public int ReturnPlayerScoreArray(int i)
+    // 플레이어 점수 배열 반환 매서드
+    public int ReturnPlayerScoreArray(int i) // index
     {
         int[] scoresArray = new int[playerScore.Count];
         int index = 0;
@@ -98,16 +96,19 @@ public class PlayerContainer : ScriptableObject
         }
     }
 
-    public void AddPlayerisDead(string playerID, bool isDead)
+    // 플레이어 사망 추가 매서드
+    public void AddPlayerisDead(string playerID, bool isDead) // (playerID, 사망)
     {
         playerisDead[playerID] = isDead;
     }
 
-    public bool ReturnPlayerisDead(string playerID)
+    // 플레이어 사망 반환 매서드
+    public bool ReturnPlayerisDead(string playerID) // (playerID)
     {
         return playerisDead[playerID];
     }
 
+    // 플레이어 사망 반환 매서드 (모두)
     public bool ReturnPlayerisDeadAll()
     {
         foreach (var kvp in playerisDead)
@@ -118,5 +119,14 @@ public class PlayerContainer : ScriptableObject
             }
         }
         return true;
+    }
+    
+    // 컨테이너 리셋 매서드
+    public void ResetContainer()
+    {
+        playerObject.Clear();
+        playerColor.Clear();
+        playerScore.Clear();
+        playerisDead.Clear();
     }
 }
