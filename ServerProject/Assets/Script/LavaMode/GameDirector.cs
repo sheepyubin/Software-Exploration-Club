095c8eab735fd083527c8e52e62702261259ed2a
spaceix <spacepay1486@gmail.com>
2024-04-07 17:13:02 +0900

    라바모드 난이도 조정fixfix

--- ServerProject/Assets/Script/LavaMode/GameDirector.cs
+++ ServerProject/Assets/Script/LavaMode/GameDirector.cs
@@ -11,15 +11,16 @@ public class GameDirector : MonoBehaviour
 
     public GameObject gearObject;
     public Animator animator;
-    
+
     public Vector2 minPosition; // 최소 위치
     public Vector2 maxPosition; // 최대 위치
-    
+
     float delta = 0.0f;
     public float span = 2.0f;
-    float originalTime;
-    public float speed = 2.0f;
-    float returnSpeed = 20.0f;
+    public float speed = 3.0f;
+
+    bool moveMode;
+    public float modeChangeTime = 3.0f;
 
     string userID;
     bool isDead;
@@ -27,11 +28,33 @@ public class GameDirector : MonoBehaviour
     {
         userID = PhotonNetwork.LocalPlayer.UserId;
         isDead = false;
+        moveMode = false;
+        maxPosition.y = minPosition.y + 10;
         playerContainer.AddPlayerisDead(userID, isDead);
-
-        originalTime = span;
     }
-
+    private void Awake()
+    {
+        StartCoroutine("SwichingMode");
+    }
+    private IEnumerator SwichingMode()
+    {
+        while (true)
+        {
+            
+            if (moveMode)
+            {
+                moveMode = false;
+                maxPosition.y = minPosition.y + 10;
+            }
+            else
+            {
+                moveMode = true;
+                maxPosition.y = minPosition.y;
+            }
+            yield return new WaitForSeconds(modeChangeTime);
+        
+        }
+    }
     // 랜덤 좌표 생성 및 프리팹 생성 함수
     private void SpawnRandomPrefab()
     {
@@ -43,7 +66,24 @@ public class GameDirector : MonoBehaviour
         // 프리팹 생성
         Instantiate(platformPrefab, transform.position + randomPosition, Quaternion.identity);
     }
+    private void MoveMode(bool mode)
+    {
+        if (mode)
+        {
+            Movement(this.speed);
+        }
+        else
+        {
+            Movement(0);
+        }
 
+    }
+    private void Movement(float speed)
+    {
+        transform.Translate(0, speed * Time.deltaTime, 0);
+        gearObject.transform.Translate(0, speed * Time.deltaTime, 0);
+        animator.SetFloat("isRotationSpeed", speed);
+    }
     // 예시로 매 프레임마다 랜덤 프리팹을 생성하는 Update 함수
     void Update()
     {
@@ -53,18 +93,7 @@ public class GameDirector : MonoBehaviour
             delta = 0.0f;
             SpawnRandomPrefab(); // 랜덤 프리팹 생성
         }
-        transform.Translate(0, this.speed * Time.deltaTime, 0);
-
-        gearObject.transform.Translate(0, this.speed * Time.deltaTime, 0);
-        animator.SetFloat("isRotationSpeed", this.speed / 2);
-
-        this.speed += Time.deltaTime / 10;
-        if (speed > returnSpeed)
-        {
-            span -= 0.1f;
-            returnSpeed += 10.0f;
-        }
-
+        MoveMode(moveMode);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
